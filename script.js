ef41519fdedfc3495494f36fdac2beb4f05a2052
Kristián Stefurak <majk123yt@gmail.com>
2022-07-29 16:02:01 +0200

    Big Update

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
+var cssStyle = document.getElementById('style');
+
+function showTime() {
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+    document.getElementById("clock").innerText = h + ":" + m + ":" + s;
+    setTimeout(showTime, 1000);
+}
+
+showTime();
+
+function setStyle(newStyle) {
+    cssStyle.href = newStyle;
+}
+
 function myFunction() {
-    var element = document.body;
-    element.classList.toggle("darkmode");
+    if (cssStyle.href.includes("lightmode.css")) {
+        setStyle("darkmode.css");
+    } else {
+        setStyle("lightmode.css");
+    }
+    document.getElementById('modeSwitch').classList.add("ani");
+
+    var audio = document.getElementById("audio");
+    audio.play();
+
+    function hamburger() {
+        if (document.getElementById('modeSwitch').classList.contains("ani")) {
+            document.getElementById('modeSwitch').classList.remove("ani");
+        }
+    }
+    setTimeout(hamburger, 1000)
 }
